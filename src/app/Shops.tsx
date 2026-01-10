1fae2215b2997f8b52ec4f1a611fe92a9bf226ff
brandonodouglas <brandondouglas247@gmail.com>
2026-01-10 17:59:44 +0000

    Added shop input box and associated text

--- src/app/Shops.tsx
+++ src/app/Shops.tsx
@@ -1,20 +1,28 @@
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { AppText } from "@/components/AppText";
 import { Shop } from "@/components/Shop";
 import ShopInput from "@/components/ShopInput";
 
 
 export default function ShopScreen() {
+  const shopCount = 0;
   return (
-    <View className="justify-center flex-1 p-4">
-      <AppText center>
-        <AppText bold>You currently have 0 shops!</AppText> 
-      </AppText>
-      <AppText center>
-        <AppText bold>Try adding some below.</AppText> 
-      </AppText>
+    <View className="p-10 m-10">
+      <Text className="text-center">You currently have {shopCount} shops.</Text>
+      <Text className="text-center p-2">Try adding some below!</Text>
       <ShopInput />
-    
+
     </View>
+
   );
 }
+
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 0,
+    borderWidth: 1,
+    padding: 0,
+    textAlign: 'center',
+  },});
--- src/components/ShopInput.tsx
+++ src/components/ShopInput.tsx
@@ -1,14 +1,29 @@
-import { View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import { AppText } from "@/components/AppText";
+import React from "react";
 
 // This is the component that the user enters their shops into
 export default function ShopInput() {
+    const [text, onChangeText] = React.useState('');
   return (
-    <View className="justify-center flex-1 p-4">
-      <AppText center>
-        <AppText bold>Shop Input stuff goes here.</AppText> 
-        
-      </AppText>
+    <View>
+     
+        <TextInput 
+        value = {text}
+        placeholder="Enter your shop here"
+        onChangeText={onChangeText}
+        style={styles.input}
+        />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 0,
+    borderWidth: 1,
+    padding: 0,
+    textAlign: 'center',
+  },
+});
