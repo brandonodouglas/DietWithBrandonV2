b0b1273ef2cbf164e73f3e3f53fdc5dd3645811f
brandonodouglas <brandondouglas247@gmail.com>
2026-01-12 10:43:09 +0000

    User can add one item to the flatlist

--- src/components/ShopInput.tsx
+++ src/components/ShopInput.tsx
@@ -1,40 +1,126 @@
-import { TextInput, View, StyleSheet, Alert, Text } from "react-native";
+import { TextInput, View, StyleSheet, Alert, Text, Button } from "react-native";
 import { AppText } from "@/components/AppText";
-import React from "react";
+import React, { useState } from "react";
+import { FlatList } from "react-native";
+
+// Component to render individual item
+
+
+type ItemProps = { name: string }
+
+// Component to render items
+
+
+const Item = ({ name }: ItemProps) => {
+    return (
+        <View style={styles.item}>
+            <Text style={{ color: 'white' }}>{name}</Text>
+        </View>
+    );
+}
+
 
 // This is the component that the user enters their shops into
 export default function ShopInput() {
-    const [text, onChangeText] = React.useState('');
-    const [shopCount, setShopCount] = React.useState(0);
+    const [shopArray, setShopArray] = useState([
+        {
+            id: '1',
+            name: 'India',
+        },
+        {
+            id: '2',
+            name: 'USA',
+        },
+        {
+            id: '3',
+            name: 'UK',
+        },
+        {
+            id: '4',
+            name: 'China',
+        },
+        {
+            id: '5',
+            name: 'Nigeria',
+        },
+        {
+            id: '6',
+            name: 'Uganda',
+        },
+    ]);
+    // The actual shop that gets added to the shop array
+    const [shop, setShop] = useState({ id: '', name: 'lol' });
+    // Counts the numhber of shosp as of when the user inputs a shop
+    const [shopCount, setShopCount] = useState(0);
+
+
+
+    const handleSubmit = () => {
+        return (Alert.alert("hety"));
+
+    }
+
+
+
     if (shopCount == 0) {
         return (
             <View>
-                <Text className="text-center p-2"> You currently have {shopCount} shops.</Text>
-                <Text className="text-center p-2"> How about adding some below?</Text>
+                <Text className="text-center p-2">You currently have {shopCount} shops added into the app.</Text>
+                <Text className="text-center p-2">Why not add some shops below? 🛒</Text>
+
                 <TextInput
-                placeholder="Enter shop here"
-                onSubmitEditing={event => {
-                    setShopCount(shopCount => shopCount + 1);
-                    Alert.alert("Added shop " + event.nativeEvent.text + " to the app.");
-                }}
-                style={styles.input}
-            />
+
+                    placeholder="Enter shop here"
+                    onSubmitEditing={event => {
+
+
+                        setShopCount(shopCount => shopCount + 1);
+                        setShopArray([...shopArray, { id: '7', name: event.nativeEvent.text }]);
+                        console.log(shopArray);
+
+
+                    }}
+                    style={styles.input}
+
+                />
+
             </View>
+
         );
+
+
     }
     return (
         <View>
 
-            <Text className="text-center p-2">You currently have {shopCount} shops.</Text>
+            <Text className="text-center p-2">You currently have {shopCount} shops added into the app.</Text>
             <TextInput
                 placeholder="Enter shop here"
                 onSubmitEditing={event => {
+                    setShop({ id: (shopArray.length++).toString(), name: event.nativeEvent.text });
+
                     setShopCount(shopCount => shopCount + 1);
-                    Alert.alert("Added shop " + event.nativeEvent.text + " to the app.");
+                    setShopArray((shopArray) => [...shopArray, shop]);
+                    shopArray.forEach(function (entry) {
+                        console.log(entry);
+                    });
+
+                    Alert.alert("Added shop " + shop + " to the app.");
                 }}
                 style={styles.input}
+
             />
+
+            <FlatList
+                data={shopArray}
+                renderItem={({ item }) => <Item name={item.name} />}
+                keyExtractor={(item) => item.id}
+            />
+
+
+
         </View>
+
     );
 }
 
@@ -45,5 +131,12 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 0,
         textAlign: 'center',
+    },
+    item: {
+        backgroundColor: 'red',
+        padding: 10,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 8,
     },
 });
